d6c1210a2c5c739e212eb79b0f20a67165ee1fe3
highpriest19 <kryptonolagunju@gmail.com>
2025-11-01 15:58:12 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the C# Program! 🎉");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int squared = number * number;
+
+        Console.WriteLine($"Nice to meet you, {name}!");
+        Console.WriteLine($"Did you know that {number} squared is {squared}?");
+
+        Console.WriteLine("Thanks for running your first C# program!");
+    }
+}
